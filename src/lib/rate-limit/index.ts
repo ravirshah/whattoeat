2b82663258d48e43a437253574a29775b9ec7c3b
Ravi Shah <ravirshah13@gmail.com>
2026-04-26 18:14:33 -0400

    obs: add assertWithinDailyCap + RateLimitClient interface (TDD)

---
+++ src/lib/rate-limit/index.ts
@@ -0,0 +1,9 @@
+export { assertWithinDailyCap } from './assert-daily-cap';
+export { makeUpstashRateLimitClient } from './client';
+export type {
+  DailyCapResult,
+  DailyCapOk,
+  DailyCapExceeded,
+  AssertDailyCapOptions,
+  RateLimitClient,
+} from './assert-daily-cap';
